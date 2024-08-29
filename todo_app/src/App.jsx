5d43a6ff5e17a8b7e0f9d31d69780a4889cf63bf
Jenny Walvoord <jennywalvoord@gmail.com>
2024-08-29 10:23:28 -0400

    Adding Acknowledgements

--- todo_app/src/App.jsx
+++ todo_app/src/App.jsx
@@ -1,3 +1,6 @@
+// The following project was adapted from React for Beginners by Code Stoic
+// Tutorial link: https://youtu.be/fu_PYQ0b-uQ?si=Gw68l_BCT31DWnnl
+
 import Header from "./components/Header";
 import Todo from "./components/Todo";
 import "./App.css";
